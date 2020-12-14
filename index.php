2ad701b1b85a8c0e5ecf15252dca580a088e0c2a
Gustavo <gustavo-bergamo@live.com>
2020-12-14 13:00:37 -0300

    Alterado rota inicial

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ if (isset($_GET['controller']) && isset($_GET['action'])) {
 	$controller = $_GET['controller'];
 	$action     = $_GET['action'];
 } else {
-	$controller = 'pages';
-	$action     = 'home';
+	$controller = 'sales';
+	$action     = 'index';
 }
 
 require_once('views/layout.php');
